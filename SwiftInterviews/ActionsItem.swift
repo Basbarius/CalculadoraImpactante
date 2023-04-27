4290bb25f28f38e367d044948296dda12c5787e7
michelpellegrin <michel.pellegrinqz@udlap.mx>
2023-04-27 17:23:35 -0600

    Finished views links

--- SwiftInterviews/ActionsItem.swift
+++ SwiftInterviews/ActionsItem.swift
@@ -64,84 +64,112 @@ struct ActionsItem: View {
                     }
                     .padding(.leading, 15)
                     VStack{
+                        NavigationLink{
+                            InfoView4()
+                        } label: {
                         Image("car")
                             .resizable()
                             .scaledToFill()
                             .frame(width: 155, height: 155)
                             .cornerRadius(10)
                             .shadow(color: .gray, radius: 10, x: -3, y: 3)
+                        }
                         Text("Carpooling")
                             .foregroundColor(.primary)
                             .font(.caption)
                     }
                     .padding(.leading, 15)
                     VStack{
+                        NavigationLink{
+                            InfoView5()
+                        } label: {
                         Image("desconectar")
                             .resizable()
                             .scaledToFill()
                             .frame(width: 155, height: 155)
                             .cornerRadius(10)
                             .shadow(color: .gray, radius: 10, x: -3, y: 3)
+                        }
                         Text("Unplug your devices")
                             .foregroundColor(.primary)
                             .font(.caption)
                     }
                     .padding(.leading, 15)
                     VStack{
+                        NavigationLink{
+                            InfoView6()
+                        } label: {
                         Image("fridge")
                             .resizable()
                             .scaledToFill()
                             .frame(width: 155, height: 155)
                             .cornerRadius(10)
                             .shadow(color: .gray, radius: 10, x: -3, y: 3)
+                        }
                         Text("Keep your fridge clean")
                             .foregroundColor(.primary)
                             .font(.caption)
                     }
                     .padding(.leading, 15)
                     VStack{
+                        NavigationLink{
+                            InfoView7()
+                        } label: {
                         Image("email")
                             .resizable()
                             .scaledToFill()
                             .frame(width: 155, height: 155)
                             .cornerRadius(10)
                             .shadow(color: .gray, radius: 10, x: -3, y: 3)
+                        }
                         Text("Empty your inbox")
                             .foregroundColor(.primary)
                             .font(.caption)
                     }
                     .padding(.leading, 15)
                     VStack{
+                        NavigationLink{
+                            InfoView8()
+                        } label: {
                         Image("reuse")
                             .resizable()
                             .scaledToFill()
                             .frame(width: 155, height: 155)
                             .cornerRadius(10)
                             .shadow(color: .gray, radius: 10, x: -3, y: 3)
+                        }
                         Text("Reuse clothes")
                             .foregroundColor(.primary)
                             .font(.caption)
                     }
                     .padding(.leading, 15)
                     VStack{
+                        NavigationLink{
+                            InfoView()
+                        } label: {
                         Image("shop")
                             .resizable()
                             .scaledToFill()
                             .frame(width: 155, height: 155)
                             .cornerRadius(10)
                             .shadow(color: .gray, radius: 10, x: -3, y: 3)
+                        }
                         Text("Use cloth bags")
                             .foregroundColor(.primary)
                             .font(.caption)
                     }
                     .padding(.leading, 15)
                     VStack{
+                        NavigationLink{
+                            InfoView9()
+                        } label: {
                         Image("food")
                             .resizable()
                             .scaledToFill()
                             .frame(width: 155, height: 155)
                             .cornerRadius(10)
                             .shadow(color: .gray, radius: 10, x: -3, y: 3)
+                        }
                         Text("Consume local products")
                             .foregroundColor(.primary)
                             .font(.caption)
