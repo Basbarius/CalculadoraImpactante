7a4d4cec102fcd5abe832922ad5a1400a8d0d19e
Equipo cido <bas.bargagli@gmail.com>
2023-04-28 10:53:33 -0600

    added user specific files to gitignore

---
+++ SwiftInterviews/BlogSelectorView.swift
@@ -0,0 +1,20 @@
+//
+//  BlogSelectorView.swift
+//  SwiftInterviews
+//
+//  Created by UDLAP10 on 28/04/23.
+//
+
+import SwiftUI
+
+struct BlogSelectorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BlogSelectorView_Previews: PreviewProvider {
+    static var previews: some View {
+        BlogSelectorView()
+    }
+}
